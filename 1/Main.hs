62bc53611e4cb991154d22cdb8a6a5ec807f3d53
Emil Snorre Alnæs <alnaes+git@protonmail.com>
2019-12-03 21:13:04 +0100

    two of 1

--- 1/Main.hs
+++ 1/Main.hs
@@ -1,11 +1,27 @@
 module Main(main) where
 
+
 fuel :: (Integral a) => a -> a
 fuel = subtract 2 . flip div 3
 
+
+fuelWithMass :: (Integral a) => a -> a
+fuelWithMass startMass = sum $ takeWhile (>0) fuelMasses
+ where
+  startFuel = fuel startMass
+  fuelMasses = startFuel : map fuel fuelMasses
+
+
+one :: (Integral a, Show a) => [a] -> IO ()
+one = print . sum . map fuel
+
+
+two :: (Integral a, Show a) => [a] ->IO ()
+two = print . sum . map fuelWithMass
+
+
 main :: IO ()
 main = do
-  masses <- lines <$> readFile "input"
-  let
-    fuel_reqs = map (fuel . read) masses
-  print $ sum fuel_reqs
+  masses <- map read . lines <$> readFile "input"
+  one masses
+  two masses
